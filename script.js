bd3a4aaede695b56436aed0728a8d09a0da406e2
elivania <elivaniasoeiro292@gmail.com>
2024-09-19 08:20:07 -0300

    aula 5

---
+++ script.js
@@ -0,0 +1,12 @@
+const avanca = document.querySelectorAll(".btn-proximo");
+
+avanca.forEach(Button =>{
+  Button.addEventListener("click" , function(){
+    const atual = document.querySelector (".ativa");
+    const proximoPasso = "psso-" + this.getattriidute("data-proximo");
+
+    atual.classList.remove("ativo");
+        document.getElementById(proximoPasso).classList.add("ativo")
+
+  }) 
+})
